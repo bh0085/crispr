9ada6486bfa803f1b886e9492bc2826232127ca4
Benjamin Holmes <ben@gliesea.local>
2013-06-26 16:33:34 -0400

    updates setup and submit scripts for release

--- query_server/submit_query.py
+++ query_server/submit_query.py
@@ -4,7 +4,7 @@ Submits a single query to the psql server from a file, "input"
 Waits for a response and prints to a file, "output"
 '''
 
-import psycopg2,os, argparse
+import psycopg2,os, argparse, sys
 
 ROOT = os.environ["HOME"]
 DATAPATH = os.path.join(ROOT,"data")
@@ -23,8 +23,26 @@ def write_sample():
     print sample_seq+"\n"
     print "to input file: {0}".format(input_file)
     
-                         
 
+global summary_template
+summary_template =  """
+SUMMARY of job: {2:06}
+INPUT SEQUENCE {1}
+
+SIMILARITY LIMIT: {3}
+DATABASE NAME: {4}
+NUMBER ROWS RETURNED {0}
+"""
+
+def create_summary(nrows = None,
+                   input_seq = None,
+                   job_number = None,
+                   table = None,
+                   limit = None):
+    global summary_template
+    return summary_template.format(nrows,input_seq,job_number,limit,table)
+                  
+       
 def main():
     '''
     reads input file
@@ -47,6 +65,9 @@ def main():
     parser.add_argument('--table','-t',dest="table",
                         default="loci10mt",type=str,
                         help="table name to store, query")
+    parser.add_argument('--summarize','-s',dest="summarize",
+                        default=False,const=True,action="store_const",
+                        help="print out summary statistics (see JOBPATH/summary.txt)") 
     args = parser.parse_args()
     
     with open(args.input) as f:
@@ -77,12 +98,31 @@ def main():
 
     cur.execute(query)
     rows = cur.fetchall()
-    for r in rows: print r
     
+    job_params = dict(
+        nrows = len(rows),
+        input_seq = query_seq,
+        job_number = job_number,
+        limit = limit,
+        table = table)
+
     with open(os.path.join(job_path, "summary.txt"),'w') as f:
-        f.writelines(["number of rows\t{0}\n".format(len(rows))])
+        f.write(create_summary(**job_params))
+    with open(os.path.join(job_path, "matches.txt"),'w') as f:
+        f.write("\n".join("\t".join(["{0}".format(e) for e in r]) for r in rows  ))
+        
+    if args.summarize:
+         with open(os.path.join(job_path, "summary.txt")) as f:
+             print f.read()
+    else:
+        sys.stdout.write(job_path)
+
     
+
+genomes = ["human", "mouse","rat","zebrafish", "arhabdopsis", "elegans"]
     
 
 if __name__ == "__main__":
     main()
+
+
