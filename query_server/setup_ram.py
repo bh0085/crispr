e7c59f60e9367be2b698590a7d1ee4c5da6b4435
ben <ben@crisp.mit.edu>
2013-07-08 15:34:16 -0400

    adds an early script to cokmpute matches offline in RAM

--- query_server/setup_ram.py
+++ query_server/setup_ram.py
@@ -3,7 +3,9 @@
 import psycopg2
 from scipy import sparse
 import argparse
-import os, numpy as np
+import os, numpy as np, pickle
+import datetime
+utcnow = datetime.datetime.utcnow
 
 
 RD_DATAROOT = "/tmp/ramdisk/crispr"
@@ -15,33 +17,81 @@ def compute_similarities(table, nlines):
 
     RD_PATH = os.path.join(RD_DATAROOT,"{0}_{1}.pickle".format(table,nlines))
 
-    cmd = "select * from {0}_sequence limit {1} ;".format(table, nlines)
+    cmd = "select * from {0}_sequence order by id limit {1};".format(table, nlines)
     print "executing: "
     print cmd
     print  
+
+    labels, dts = [], []
+    dts.append(utcnow())
+    labels.append("start")
+
     cur.execute(cmd)
 
-    print "executed"
+    dts.append(utcnow())
+    labels.append("executed")
+
     lines = cur.fetchall()
+    dts.append(utcnow())
+    labels.append("fetched")
+
     translation = {"A":0,
                    "G":1,
                    "T":2,
                    "C":3}
-    all_seqs = np.array( [[translation[let] for let in e[1]] for e in lines])
+    max_lines = len(lines)
+    letters = 20
+
+    all_seqs = np.zeros((max_lines, 20), np.int8)
+    dts.append(utcnow())
+    labels.append("allocated array")
+
+
+    #enter only some lines... change later
+    for l in lines:
+        if l[0] < max_lines:
+            all_seqs[l[0] - 1] = [translation[let] for let in l[1]]
+
+    dts.append(utcnow())
+    labels.append("populated array")
+
+
     min_matches = 15
-    lil_sparse = sparse.lil.lil_matrix( len(all_seqs), len(all_seqs) )
-    for i in range(1): #len(all_seqs)):
-        sims_array = np.sum(np.equal(all_seqs[i][np.newaxis,:] - all_seqs[:,:], 0),1)
-        hits_by_spacer = np.nonzero(np.greater_equal(sims_array,min_matches))
-    print len(hits_by_spacer)
+    ns = len(all_seqs)
+    lils = sparse.lil_matrix(( ns ,ns ))
+    #for i in range(ns):
+
+    sims_array = np.sum(np.equal(all_seqs[:,np.newaxis,:] - all_seqs[np.newaxis,:,:], 0),2)
+    print sims_array.shape
+    #nz = np.nonzero(np.greater_equal(sims_array,min_matches))
+    #print nz.shape
+
+    lils[0,0] = sims_array[0,0]
+    #    if i % (ns/3) == 0:
+    #        print "{0:05} ({1}/{2})".format(float(i) / len(all_seqs), i, len(all_seqs))
+
+    dts.append(utcnow())
+    labels.append("ran through {0} sims".format(len(all_seqs)))
+
+    with open(RD_PATH, 'w') as f:
+        pickle.dump(lils, f)
+
+    dts.append(utcnow())
+    labels.append("wrote pickle output")
+
+    for i in range(len(dts) -1):
+        print "seconds to {1}: {0}".format((dts[i+1] - dts[i]), labels[i+1])
+        
+    print "microseconds per item: {0}".format(((dts[5] - dts[4]).seconds * 1e6 +(dts[5] - dts[4]).microseconds)  / len(all_seqs))
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--nlines','-n',dest="nlines",
-                        default=10000,type=int,
+                        default=1000,type=int,
                         help="number of lines to enter into a sparse similarity matrix")
     parser.add_argument('--table','-t',dest="table",
-                        default="all_ram",type=str,
+                        default="locs10mt_ram",type=str,
                         help="table name to query")
     args = parser.parse_args()
     global conn, cur, locsfile
