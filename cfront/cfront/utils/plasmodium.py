3988d4ef0ae5140e7f085c4142ee3ab71014704b
ben <crispr.mit.edu>
2014-03-12 20:49:37 -0400

    adds in the first plasmo file

---
+++ cfront/cfront/utils/plasmodium.py
@@ -0,0 +1,11 @@
+'''
+plain and simple full genome scan pipeline for alejandra's
+plasmodium genes of interest
+'''
+
+import os
+
+organism_root = "/data/plasmodium"
+genome_file = os.path.join(organism_root, "genome.fa")
+genes_file = os.path.join(organism_root, "genes.fa")
+
